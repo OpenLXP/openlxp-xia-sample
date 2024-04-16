74e7e9a1562621ac57b04a289c4253ca1326f223
kjijo <kjijo@deloitte.com>
2024-04-16 16:13:14 -0500

    Updating sample

--- openlxp-xia-sample/start-app.sh
+++ openlxp-xia-sample/start-app.sh
@@ -2,6 +2,9 @@
 
 python manage.py waitdb 
 python manage.py migrate 
+python manage.py createcachetable 
+python manage.py loaddata admin_theme_data.json 
+python manage.py loaddata xiaconfiguration.json 
 cd /opt/app/ 
 pwd 
 ./start-server.sh
